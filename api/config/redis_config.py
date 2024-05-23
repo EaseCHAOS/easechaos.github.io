3aa69f020374421f1cf0c3d262938b3f5e85280d
Dompeh Kofi Bright <kekelidompeh@gmail.com>
2024-05-23 13:58:41 +0000

    chore: cleanup

--- api/config/redis_config.py
+++ api/config/redis_config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 r = redis.Redis(
-    host=os.environ.get("REDIS_HOST"),
+    host=os.environ.get("REDIS_HOST"), 
     port=os.environ.get("REDIS_PORT"),
     password=os.environ.get("REDIS_PASSWORD"),
     ssl=True,
